0d413f682c304e4dcb823208f9467c47cc7d702a
Samvel <loidnoir@gmail.com>
2023-07-26 11:54:36 +0400

    feat: db change

--- src/events/readyEvent.ts
+++ src/events/readyEvent.ts
@@ -2,7 +2,6 @@ import contactCommandData from '@commands/contact/data'
 import helpCommandData from '@commands/help/data'
 import joinCommandData from '@commands/join/data'
 import playCommandData from '@commands/play/data'
-import testCommandData from '@commands/test/data'
 import config from '@constants/config'
 import { consoleSuccess } from '@helpers/loggers'
 import GameClient from '@structures/Client'
@@ -12,8 +11,8 @@ export default async function(client: GameClient) {
   await client.application?.commands.create(playCommandData)
   await client.application?.commands.create(joinCommandData)
   await client.application?.commands.create(helpCommandData)
-  await client.guilds.cache.get(config.guild)?.commands.create(testCommandData)
   await client.guilds.cache.get(config.guild)?.commands.create(contactCommandData)
+  // await client.guilds.cache.get(config.guild)?.commands.create(testCommandData)
 
   client.user?.setActivity({
     name: `for ${client.games.size} games`,
